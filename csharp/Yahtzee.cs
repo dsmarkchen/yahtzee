1ef74b01c62a8c6d6eb8658b17c299fd5902c47c
Mark <dsmarkchen@gmail.com>
2013-07-26 18:04:28 -0600

    init copy of yahtzee developed on C# ms visual studio 2012

---
+++ csharp/Yahtzee.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+public class Dice
+{
+    Random rand;
+    public Dice() { 
+        rand = new Random();
+
+    }
+    public Dice(Random ra) {
+        rand = ra;
+    }
+    public int Roll() {
+        return rand.Next(1,7);
+    }
+}
+
+public class Yahtzee
+{
+    Random rand;
+
+    public const byte SCORE_T_CHANCE = 0; 
+    public const byte SCORE_T_YAHTZEE = 1; 
+    public const byte SCORE_T_ONES = 2; 
+    public const byte SCORE_T_PAIR = 3; 
+    public const byte SCORE_T_TWOPAIR = 4; 
+    public const byte SCORE_T_THREE = 5; 
+    public const byte SCORE_T_FOUR = 6;
+    public const byte SCORE_T_STRAIGHT = 7;
+    public const byte SCORE_T_FULLHOUSE = 8;
+
+    private Dice[] dice = null;
+    public Yahtzee() {
+        rand = new Random();
+        dice = new Dice[5];
+        for(int i=0;i<5;i++) {
+            dice[i] = new Dice(rand);
+        }
+    }
+    public  void play(out int[] scores){
+        scores = new int[5];
+        for(int i=0;i<5;i++)
+            scores[i] = dice[i].Roll();
+    }
+
+    private int yahtzee_score(int[] scores) 
+    {
+        if((scores[0] == scores[1]) &&  
+                (scores[0] == scores[2])  &&
+                (scores[0] == scores[3])  &&
+                (scores[0] == scores[4])  )  {
+            return 50;
+        }
+        return 0;
+    }
+    private int ones_score(int[] scores, int x0)
+    {
+        int j=0;
+        for(int i=0;i<5;i++) 
+            if(x0==scores[i]) j++;
+        return j*x0;
+    }
+    private int pair_score(int[] scores)
+    {
+        int[] cnt = new int [6] {0,0,0,0,0,0};
+
+
+        for(int j=0;j<6;j++) {
+            for(int i=0;i<5;i++) {
+                if((j+1)==scores[i]) cnt[j]++;
+            }
+
+        }
+        int r = 0;
+        for(int j=0;j<6;j++) {
+            if(cnt[j] == 2)  r=(j+1)*2;
+        }
+        return r;
+    }
+    private int twopair_score(int[] scores)
+    {
+        int[] cnt = new int [6] {0,0,0,0,0,0};
+
+
+        for(int j=0;j<6;j++) {
+            for(int i=0;i<5;i++) {
+                if((j+1)==scores[i]) cnt[j]++;
+            }
+
+        }
+        int r = 0;
+        int k = 0;
+        for(int j=0;j<6;j++) {
+            if(cnt[j] == 2) { 
+                k++;
+                r+=(j+1)*2;
+            }
+        }
+        if(k!=2) return 0;
+        return r;
+    }
+    private int three_score(int[] scores)
+    {
+        int[] cnt = new int [6] {0,0,0,0,0,0};
+
+
+        for(int j=0;j<6;j++) {
+            for(int i=0;i<5;i++) {
+                if((j+1)==scores[i]) cnt[j]++;
+            }
+
+        }
+        int r = 0;
+        for(int j=0;j<6;j++) {
+            if(cnt[j] == 3)  r=(j+1)*3;
+        }
+        return r;
+    }
+    private int four_score(int[] scores)
+    {
+        int[] cnt = new int [6] {0,0,0,0,0,0};
+
+
+        for(int j=0;j<6;j++) {
+            for(int i=0;i<5;i++) {
+                if((j+1)==scores[i]) cnt[j]++;
+            }
+
+        }
+        int r = 0;
+        for(int j=0;j<6;j++) {
+            if(cnt[j] == 4)  r=(j+1)*4;
+        }
+        return r;
+    }
+        public int score(int[] scores, byte score_type = SCORE_T_CHANCE, int x0 = 1) 
+    {
+        int yahtzees = yahtzee_score(scores);
+        if(score_type == SCORE_T_YAHTZEE) return yahtzees;
+        
+        int ones = ones_score(scores, x0);
+        if(score_type == SCORE_T_ONES) return ones;
+        
+        int pair = pair_score(scores);
+        if(score_type == SCORE_T_PAIR) return pair;
+    
+        int pairs = twopair_score(scores);
+        if(score_type == SCORE_T_TWOPAIR) return pairs;
+
+        int three_sum = three_score(scores);
+        if(score_type == SCORE_T_THREE) return three_sum;
+
+        int four_sum = four_score(scores);
+        if(score_type == SCORE_T_FOUR) return four_sum;
+
+        int fullhouse = 0;
+        if(pair != 0 && three_sum !=0) {
+            fullhouse = pair + three_sum;
+         }     
+         if(score_type == SCORE_T_FULLHOUSE) return fullhouse; 
+           
+          int sum = 0;
+        for(int i=0;i<5;i++)
+            sum += scores[i];
+        return sum;
+
+    }
+}
+
+
---
+++ csharp/YahtzeeTester.cs
@@ -0,0 +1,277 @@
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using NUnit.Framework;
+//using Yahtzee;
+
+[TestFixture]
+public class DiceTester 
+{
+    [Test]
+    public void Dice_DiceRoll100Times_GetNumberBetween1to6()
+    {
+        int r = 0;
+        int n;
+        Dice o = new Dice();
+        for (int i = 0; i < 100; i++)
+        {
+            n = o.Roll();
+            if ((n < 1) || (n > 6)) r++;
+#if DEBUG
+            Console.WriteLine(n.ToString());
+#endif
+        }
+
+        Assert.AreEqual(0, r);
+    }
+
+    [Test]
+    public void Dice_DiceRoll100Times_GetEachNumberAtleastOnce()
+    {
+        int r1 = 0;
+        int r2 = 0;
+        int r3 = 0;
+        int r4 = 0;
+        int r5 = 0;
+        int r6 = 0;
+        int n;
+        Dice o = new Dice();
+        for (int i = 0; i < 100; i++)
+        {
+            n = o.Roll();
+            if ((n == 1)) r1++;
+            if ((n == 2)) r2++;
+            if ((n == 3)) r3++;
+            if ((n == 4)) r4++;
+            if ((n == 5)) r5++;
+            if ((n == 6)) r6++;
+        }
+
+        Assert.IsTrue(r1>0);
+        Assert.IsTrue(r2>0);
+        Assert.IsTrue(r3>0);
+        Assert.IsTrue(r4>0);
+        Assert.IsTrue(r5>0);
+        Assert.IsTrue(r6>0);
+    }
+}
+
+
+[TestFixture]
+public class YahtzeeTester
+{
+    [Test]
+    public void Yahtzee_PlayTwice_ScoreDifferently()
+    {
+        int[] scores;
+        int[] scores2;
+        Yahtzee o = new Yahtzee();
+        o.play(out scores);
+        o.play(out scores2);
+        Assert.AreNotEqual(scores, scores2);
+    }
+
+    [Test]
+    public void Yahtzee_ChanceScore_1_1_3_3_6_score_14()
+    {
+        int[] scores = {1,1,3,3,6};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores);
+        Assert.AreEqual(14, n);
+
+    }
+    [Test]
+    public void Yahtzee_ChanceScore_4_5_5_6_1_score_21()
+    {
+        int[] scores = {4,5,5,6,1};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores);
+        Assert.AreEqual(21, n);
+
+    }
+
+    [Test]
+    public void Yahtzee_YahtzeeScore_1_1_1_1_1_score_50()
+    {
+        int[] scores = {1,1,1,1,1};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_YAHTZEE);
+        Assert.AreEqual(50, n);
+
+    }
+    [Test]
+    public void Yahtzee_YahtzeeScore_1_1_1_2_1_score_0()
+    {
+        int[] scores = {1,1,1,2,1};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_YAHTZEE);
+        Assert.AreEqual(0, n);
+
+    }
+
+    [Test]
+    public void Yahtzee_OnesScore_1_1_2_4_4_score_8_on_four()
+    {
+        int[] scores = {1,1,2,4,4};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_ONES, 4);
+        Assert.AreEqual(8, n);
+
+    }
+    [Test]
+    public void Yahtzee_OnesScore_2_3_2_5_1_score_4_on_two()
+    {
+        int[] scores = {2,3,2,5,1};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_ONES, 2);
+        Assert.AreEqual(4, n);
+
+    }
+    [Test]
+    public void Yahtzee_OnesScore_3_3_3_4_5_score_0_on_one()
+    {
+        int[] scores = {3,3,3,4,5,};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_ONES, 1);
+        Assert.AreEqual(0, n);
+
+    }
+
+    [Test]
+    public void Yahtzee_pairscore_3_3_3_4_4_score_8()
+    {
+        int[] scores = {3,3,3,4,4,};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_PAIR);
+        Assert.AreEqual(8, n);
+    }
+    [Test]
+    public void Yahtzee_pairscore_1_1_6_2_6_score_12()
+    {
+        int[] scores = {1 ,1,6,2,6};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_PAIR);
+        Assert.AreEqual(12, n);
+    }
+    [Test]
+    public void Yahtzee_pairscore_3_3_3_4_1_score_0()
+    {
+        int[] scores = { 3, 3, 3, 4, 1 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_PAIR);
+        Assert.AreEqual(0, n);
+    }
+    [Test]
+    public void Yahtzee_pairscore_3_3_3_3_1_score_0()
+    {
+        int[] scores = { 3, 3, 3, 3, 1 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_PAIR);
+        Assert.AreEqual(0, n);
+    }
+    [Test]
+    public void Yahtzee_twopairscore_1_1_2_3_3_score_8()
+    {
+        int[] scores = {1,1,2,3,3};
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_TWOPAIR);
+        Assert.AreEqual(8, n);
+    }
+
+    [Test]
+    public void Yahtzee_twopairscore_1_1_2_3_4_score_0()
+    {
+        int[] scores = { 1, 1, 2, 3, 4 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_TWOPAIR);
+        Assert.AreEqual(0, n);
+    }
+    [Test]
+    public void Yahtzee_twopairscore_1_1_2_2_2_score_0()
+    {
+        int[] scores = { 1, 1, 2, 2, 2 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_TWOPAIR);
+        Assert.AreEqual(0, n);
+    }
+    [Test]
+    public void Yahtzee_threescore_3_3_3_4_5_score_9()
+    {
+        int[] scores = { 3, 3, 3, 4, 5 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_THREE);
+        Assert.AreEqual(9, n);
+    }
+    [Test]
+    public void Yahtzee_threescore_3_3_4_5_6_score_0()
+    {
+        int[] scores = { 3, 3, 4, 5, 6 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_THREE);
+        Assert.AreEqual(0, n);
+    }
+    [Test]
+    public void Yahtzee_threescore_3_3_3_3_1_score_0()
+    {
+        int[] scores = { 3, 3, 3, 3, 1 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_THREE);
+        Assert.AreEqual(0, n);
+    }
+  
+    [Test]
+    public void Yahtzee_fourscore_2_2_2_2_5_score_8()
+    {
+        int[] scores = { 2, 2, 2, 2, 5 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_FOUR);
+        Assert.AreEqual(8, n);
+    }
+    [Test]
+    public void Yahtzee_smallstraight_score_15()
+    {
+
+        int[] scores = { 1, 2, 3, 4, 5 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_STRAIGHT);
+        Assert.AreEqual(15, n);
+    }
+    [Test]
+    public void Yahtzee_largestraight_score_20()
+    {
+
+        int[] scores = { 2, 3, 4, 5, 6 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_STRAIGHT);
+        Assert.AreEqual(20, n);
+    }
+    [Test]
+    public void Yahtzee_fullhouse_1_1_2_2_2_score_8()
+    {
+
+        int[] scores = { 1, 1, 2, 2, 2 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_FULLHOUSE);
+        Assert.AreEqual(8, n);
+    }
+    [Test]
+    public void Yahtzee_fullhouse_2_2_3_3_4_score_0()
+    {
+
+        int[] scores = { 2, 2, 3, 3, 4 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_FULLHOUSE);
+        Assert.AreEqual(0, n);
+    }
+
+    [Test]
+    public void Yahtzee_fullhouse_4_4_4_4_4_score_0()
+    {
+
+        int[] scores = { 4, 4, 4, 4, 4 };
+        Yahtzee o = new Yahtzee();
+        int n = o.score(scores, Yahtzee.SCORE_T_FULLHOUSE);
+        Assert.AreEqual(0, n);
+    }
+}
